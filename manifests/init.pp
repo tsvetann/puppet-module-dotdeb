93fe90b916c45032f251cd9a7d4cf23ed0525d62
tsvetann <tsvetann@users.noreply.github.com>
2015-03-11 18:07:23 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class dotdeb($key = 'http://www.dotdeb.org/dotdeb.gpg', $lenny_php53 = '') {
+class dotdeb($key = 'http://www.dotdeb.org/dotdeb.gpg') {
     class{ "dotdeb::install":
     } -> anchor { 'dotdeb::end': }
 }
